f1afbbc7948f47247d25dbac26495669445afc13
ChaitraDinesh <Dinesh.Chaitra@in.bosch.com>
2024-10-28 14:43:46 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -1,1 +1,16 @@
+#include "StringCalculator.h"
+#include <gtest/gtest.h>
 
+TEST(string_calculator_add, when_passes_a_single_number_returns_0_for_zero)
+{
+  //Arrange
+  stringCalculator objunderTest;
+  string input = "";
+  int expectedValue = 0;
+
+  //Act
+  int actualValue = objunderTest.Add(input);
+
+  //Assert
+  ASSERT_EQ(expectedValue, actualValue);
+}
