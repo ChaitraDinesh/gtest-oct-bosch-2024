7c18f12447a8097fc7b29feff72d7f82b821584c
ChaitraDinesh <Dinesh.Chaitra@in.bosch.com>
2024-10-29 11:49:16 +0530

    Update TestChallenge.Tests.cpp

--- TestChallenge.Tests.cpp
+++ TestChallenge.Tests.cpp
@@ -1,1 +1,14 @@
-
+#include "TestChallenge.h"
+#include <gtest/gtest.h>
+ 
+TEST(AddTest, CorrectSum) {
+    int result = 0;
+    add(5, 3, &result);
+    EXPECT_EQ(result, 8);
+}
+ 
+TEST(AddTest, IncorrectOutput) {
+    int result = 0;
+    add(5, 3, &result);
+    EXPECT_EQ(to_string(result), "Sum of 5:3 is 8");
+}
