c55ffa0f4a5b9bdff1f635e8950eb4f1942ae6db
ChaitraDinesh <Dinesh.Chaitra@in.bosch.com>
2024-10-29 15:35:24 +0530

    Update VehicleSpeedMonitor.h

--- VehicleSpeedMonitor.h
+++ VehicleSpeedMonitor.h
@@ -1,22 +1,16 @@
 #include <iostream>
 #include <random> 
 #include <string>
+#include "ILogger.h"
+#include "IMessageCommunicator.h"
+#inlcude "ISpeedSensor.h"
 using namespace std;
-
-class BNFSpeedSensor{
-        public:
-            int   getCurrentSpeed(){
-                std::random_device rd;
-                std::mt19937 gen(rd());
-                std::uniform_int_distribution<> distr(1, 100);
-                return distr(gen);
-            }
-};
 
 class SpeedMonitor{
     private : int _speedThreshold;
   
       public:
           SpeedMonitor(int speedThreshold): _speedThreshold{speedThreshold} {}
-          void monitor();
+          //Dependency Injection
+          void monitor(IMessageCommunicator* communicator, ISpeedSensor* sensor, ILogger* logger);
 };
